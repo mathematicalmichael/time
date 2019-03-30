8a7edd31c7034a6da21bd663fdfb4c8397036cb3
Michael Pilosov <consistentbayes@gmail.com>
2019-03-30 00:15:10 -0600

    update.

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 ./gen_README.sh
 git commit -am 'auto commit from math.computer' 
 
-git push origin master && \
+git push origin self && \
 	echo "Successfully updated and synced public repository."
 
